65a856d72354d0431708d07edc95935ed3d72591
Andrey Shendrik <lc811@yandex.ru>
2020-07-13 14:09:32 +0300

    v 1.01 changelog: - some code refactoring (names, duplicated fragments removed, event loop simplification); - file operations have been moved to the context Manager; - game speed fixed with time.sleep() function

--- main.py
+++ main.py
@@ -1,30 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
 import curses
 import asyncio
 import random
 from itertools import cycle
-
+from time import sleep
 
 SPACE_KEY_CODE = 32
 LEFT_KEY_CODE = 260
 RIGHT_KEY_CODE = 261
 UP_KEY_CODE = 259
 DOWN_KEY_CODE = 258
-
 STARS = '+*.:'
 
-f1 = open('./data/frame1.txt', 'r')
-f2 = open('./data/frame2.txt', 'r')
-frame1 = f1.read()
-frame2 = f2.read()
-frames = [frame1, frame2]
-f1.close()
-f2.close()
-
-SPACE_KEY_CODE = 32
-LEFT_KEY_CODE = 260
-RIGHT_KEY_CODE = 261
-UP_KEY_CODE = 259
-DOWN_KEY_CODE = 258
+with open('./data/frame1.txt', 'r') as f1, open('./data/frame2.txt', 'r') as f2:
+    frame1 = f1.read()
+    frame2 = f2.read()
+    frames = [frame1, frame2]
 
 
 def read_controls(canvas):
@@ -99,18 +92,20 @@ def draw_frame(canvas, start_row, start_column, text, negative=False):
             canvas.addch(row, column, symbol)
 
 
-def get_stars(canvas, ROWS, COLS, min_stars=30, max_stars=70):
+def get_stars(canvas, rows, cols, min_stars=30, max_stars=70):
     coroutines = []
-    for i in range(random.randint(min_stars, max_stars)):
-        coroutines.append(blink(canvas,
-                        random.randint(1, ROWS - 1),
-                        random.randint(1, COLS - 1),
-                        symbol=random.choice(STARS),
-                        TIC_TIMEOUT=random.random()))
+    stars_count = random.randint(min_stars, max_stars)
+    for i in range(stars_count):
+        coroutine = blink(canvas,
+                          random.randint(1, rows - 1),
+                          random.randint(1, cols - 1),
+                          symbol=random.choice(STARS),
+                          tic_timeout=random.random())
+        coroutines.append(coroutine)
     return coroutines
 
 
-async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+async def fire(canvas, start_row, start_column, rows_speed=-0.1, columns_speed=0):
     """Display animation of gun shot, direction and speed can be specified."""
 
     row, column = start_row, start_column
@@ -141,55 +136,55 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
 
 
 def constraint_spaceship_position(canvas, frame, spaceship_pos_rows, spaceship_pos_cols):
-    """Constraint spaceship position to prevent desertion from the battlefield with space debris"""
+    """Constraint spaceship position to prevent desertion from the battlefield with space debris."""
 
     canvas_height, canvas_width = canvas.getmaxyx()
     frame_height, frame_width = get_frame_size(frame)
     field_height = canvas_height - frame_height
     field_width = canvas_width - frame_width
-    rows = min(field_height, (max((0, spaceship_pos_rows))))
-    cols = min(field_width, max((0, spaceship_pos_cols)))
+    rows = min(field_height, max(0, spaceship_pos_rows))
+    cols = min(field_width, max(0, spaceship_pos_cols))
     return rows, cols
 
 
-async def animate_spaceship(canvas, row, column, TIC_TIMEOUT=1):
-        spaceship_pos_rows = row
-        spaceship_pos_cols = column
-        frames_iter = cycle(frames)
-        while True:
-            frame = next(frames_iter)
-            spaceship_pos_rows, spaceship_pos_cols = constraint_spaceship_position(canvas, frame,
-                                                                                   spaceship_pos_rows,
-                                                                                   spaceship_pos_cols)
-            # Сохраняем положение кадра анимации, потребуется ниже для очистки экрана
-            previous_rows = spaceship_pos_rows
-            previous_cols = spaceship_pos_cols
-            draw_frame(canvas, spaceship_pos_rows, spaceship_pos_cols, frame)
-            for i in range(int(500 * TIC_TIMEOUT)):
-                rows_direction, columns_direction, space_pressed = read_controls(canvas)
-                spaceship_pos_rows += rows_direction
-                spaceship_pos_cols += columns_direction
-                await asyncio.sleep(0)
-            # Очищаем экран от "старого" кадра анимации
-            draw_frame(canvas, previous_rows, previous_cols, frame, negative=True)
+async def animate_spaceship(canvas, row, column, tic_timeout=1):
+    spaceship_pos_rows = row
+    spaceship_pos_cols = column
+    frames_iter = cycle(frames)
+    while True:
+        frame = next(frames_iter)
+        spaceship_pos_rows, spaceship_pos_cols = constraint_spaceship_position(canvas, frame,
+                                                                               spaceship_pos_rows,
+                                                                               spaceship_pos_cols)
+        # Сохраняем положение кадра анимации, потребуется ниже для очистки экрана
+        previous_rows = spaceship_pos_rows
+        previous_cols = spaceship_pos_cols
+        draw_frame(canvas, spaceship_pos_rows, spaceship_pos_cols, frame)
+        for i in range(int(500 * tic_timeout)):
+            rows_direction, columns_direction, space_pressed = read_controls(canvas)
+            spaceship_pos_rows += rows_direction
+            spaceship_pos_cols += columns_direction
+            await asyncio.sleep(0)
+        # Очищаем экран от "старого" кадра анимации
+        draw_frame(canvas, previous_rows, previous_cols, frame, negative=True)
 
 
-async def blink(canvas, row, column, symbol='*', TIC_TIMEOUT=1):
+async def blink(canvas, row, column, symbol='*', tic_timeout=1):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        for i in range(int(1000 * 20 * TIC_TIMEOUT)):
+        for i in range(int(1000 * 20 * tic_timeout)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        for i in range(int(1000 * 3 * TIC_TIMEOUT)):
+        for i in range(int(1000 * 3 * tic_timeout)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        for i in range(int(1000 * 5 * TIC_TIMEOUT)):
+        for i in range(int(1000 * 5 * tic_timeout)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        for i in range(int(1000 * 3 * TIC_TIMEOUT)):
+        for i in range(int(1000 * 3 * tic_timeout)):
             await asyncio.sleep(0)
 
 
@@ -198,16 +193,15 @@ def draw(canvas):
     curses.curs_set(False)
     canvas.nodelay(True)
     coroutines = get_stars(canvas, canvas_height, canvas_width, min_stars=100, max_stars=250)
-    coroutines.append(fire(canvas, start_row=canvas_height//2, start_column=canvas_width//2, rows_speed=-0.008))
-    coroutines.append(animate_spaceship(canvas, canvas_height//2, canvas_width//2, TIC_TIMEOUT=0.3))
-    while True:
+    coroutines.append(fire(canvas, start_row=canvas_height // 2, start_column=canvas_width // 2, rows_speed=-0.05))
+    coroutines.append(animate_spaceship(canvas, canvas_height // 2, canvas_width // 2, tic_timeout=0.3))
+    while len(coroutines):
+        sleep(0.001)
         for coroutine in coroutines.copy():
             try:
                 coroutine.send(None)
             except StopIteration:
                 coroutines.remove(coroutine)
-            if len(coroutines) == 0:
-                break
         canvas.refresh()
 
 
